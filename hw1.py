9a521246b86c89348cb7cdd0a9f4751d35eef44d
kimhxsong <kimhxsong@gmail.com>
2024-03-19 16:32:49 +0900

    done: homework1

---
+++ hw1.py
@@ -0,0 +1,45 @@
+import cv2
+
+def main():
+    cap1 = cv2.VideoCapture('./assets/lec5_video1.mp4')
+    cap2 = cv2.VideoCapture('./assets/lec5_video2.mp4')
+
+    fps = cap1.get(cv2.CAP_PROP_FPS)
+    shape = round(cap1.get(cv2.CAP_PROP_FRAME_WIDTH)), round(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter.fourcc(*'XVID')  # Changed codec to 'mp4v'
+    output = cv2.VideoWriter('./lec5_video_output.avi', fourcc, fps, shape)  # Changed file extension to .mp4
+
+    count = 1
+    ret1, frame1 = cap1.read()
+    while ret1 and frame1 is not None and count <= 24:
+        output.write(frame1)
+
+        ret1, frame1 = cap1.read()
+        count += 1
+
+    step = (shape[0] / 48)
+    ret2, frame2 = cap2.read()
+    while count <= (24 * 3):  # FPS * SEC
+        transition_frame = frame1.copy()
+        transition_frame[:, -int(step * (count - 24)):] = frame2[:, -int(step * (count - 24)):]
+        output.write(transition_frame)
+
+        ret1, frame1 = cap1.read()
+        ret2, frame2 = cap2.read()
+        count += 1
+
+    ret2, frame2 = cap2.read()
+    while ret2 and frame2 is not None and count <= 24 * 6:
+        output.write(frame2)
+
+        ret2, frame2 = cap2.read()
+        count += 1
+    print(shape)
+    cap1.release()
+    cap2.release()
+    output.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
